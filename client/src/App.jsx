1b678a8c89ac5ff35f38f898d91c639533c55759
johnannysker <john-ao@hotmail.com>
2026-04-28 21:17:09 -0300

    Criação do projeto comun_ação

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind funcionando 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
